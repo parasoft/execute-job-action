b622927c96a01e9a00805bfac36e820295798432
humphrey <humphrey@parasoft.com>
2021-01-27 18:18:23 -0800

    Mock REST API calls in GitHub execute-job-action tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,25 +1,76 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as core from '@actions/core';
+import * as main from '../src/main';
+import * as service from '../src/service';
+import q from 'q';
+import http from 'http';
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_CTPURL'] = 'http://104.42.225.105/em'
-  process.env['INPUT_CTPUSERNAME'] = 'admin'
-  process.env['INPUT_CTPPASSWORD'] = 'admin'
-  process.env['INPUT_CTPJOB'] = 'Status Job'
-  process.env['INPUT_PUBLISHREPORT'] = 'true'
-  process.env['INPUT_DTPURL'] = 'http://54.149.98.67:8080'
-  process.env['INPUT_DTPUSERNAME'] = 'demo'
-  process.env['INPUT_DTPPASSWORD'] = 'demo-user'
-  process.env['INPUT_DTPPROJECT'] = 'Parabank_Master'
-  process.env['INPUT_BUILDID'] = 'Parabank2021-01-21'
-  process.env['INPUT_SESSIONTAG'] = 'Parabank-Github-Action'
-  process.env['INPUT_APPENDENVIRONMENT'] = 'true'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
-})
+const jobsResponse =  {"jobs":[{"context":{},"fork":false,"id":20,"name":"fake-job","testConfiguration":"","testScenarioInstances":[{"dataGroups":[{"active":"${NUMBER_SET}","id":"ds_-54812465_1551241335538_1013998772"}],"id":12,"priority":0,"testScenarioId":4,"variableSet":"Smoke Test","variables":[{"key":"ENDPOINT","type":"active"},{"key":"NUMBER_SET","type":"active"},{"key":"PASSWORD","type":"active"},{"key":"USERNAME","type":"active"},{"key":"WSDL","type":"active"}]}]},{"context":{},"fork":false,"id":20,"name":"Parabank Job","testConfiguration":"","testScenarioInstances":[{"dataGroups":[],"id":18,"priority":0,"testScenarioId":5,"variables":[{"key":"ACCOUNT","type":"active"},{"key":"BASEURL","type":"active"},{"key":"SWAGGER","type":"active"}]}]}]};
+const jobHistoryResponse_WAITING = {"context":{},"id":12,"jobId":20,"jobName":"Parabank Job","status":"WAITING","testExecutionIds":[],"testHistories":[],"username": ""}
+const jobHistoryResponse_RUNNING = {"context":{},"id":12,"jobId":20,"jobName":"Parabank Job","startTime":1610748281466,"status":"RUNNING","testExecutionIds":["801757114"],"testHistories":[],"username": ""}
+const jobHistoryResponse_SUCCESS = {"completionTime":1610748286577,"context":{},"id":12,"jobId":20,"jobName":"Parabank Job","reportIds":[69],"startTime":1610748281466,"status":"PASSED","testExecutionIds":["801757114"],"testHistories":[{"instanceId":18,"name":"Parabank","reportId":69,"status":"PASSED","testScenarioId":5}],"username":""}
+
+test('test basic execute job scenario', () => {
+    jest.spyOn(core, 'getInput').mockImplementation((val) => {
+        if (val === 'ctpUrl') {
+            return 'https://fake-ctp-endpoint:8080/em/'
+        } else if (val === 'ctpUsername' || val === 'ctpPassword') {
+            return 'admin';
+        } else if ('ctpJob') {
+            return 'fake-job'
+        }
+    });
+    jest.spyOn(service.WebService.prototype, 'performPOST').mockImplementation((path, data) => {
+        let def = q.defer();
+        let promise = new Promise((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        console.log('mock-performPOST invoked');
+        if (path === '/api/v2/jobs/20/histories?async=true') {
+            var res = new http.IncomingMessage(null);
+            res.statusCode = 200;
+            def.resolve(jobHistoryResponse_SUCCESS);
+        }
+        return promise
+    });
+    jest.spyOn(service.WebService.prototype, 'performGET').mockImplementation((path, handler, dataHandler) => {
+        let def = q.defer();
+        let promise = new Promise((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        console.log('mock-performGET invoked');
+        if (path === '/api/v2/jobs') {
+            let res = new http.IncomingMessage(null);
+            res.statusCode = 200;
+            handler(res, def, JSON.stringify(jobsResponse));
+        }
+        return promise;
+    });
+    main.run();
+});
+
+// // shows how the runner will run a javascript action with env / stdout protocol
+// test('test runs', () => {
+//   process.env['INPUT_CTPURL'] = 'http://104.42.225.105/em'
+//   process.env['INPUT_CTPUSERNAME'] = 'admin'
+//   process.env['INPUT_CTPPASSWORD'] = 'admin'
+//   process.env['INPUT_CTPJOB'] = 'Status Job'
+//   process.env['INPUT_PUBLISHREPORT'] = 'true'
+//   process.env['INPUT_DTPURL'] = 'http://54.149.98.67:8080'
+//   process.env['INPUT_DTPUSERNAME'] = 'demo'
+//   process.env['INPUT_DTPPASSWORD'] = 'demo-user'
+//   process.env['INPUT_DTPPROJECT'] = 'Parabank_Master'
+//   process.env['INPUT_BUILDID'] = 'Parabank2021-01-21'
+//   process.env['INPUT_SESSIONTAG'] = 'Parabank-Github-Action'
+//   process.env['INPUT_APPENDENVIRONMENTSET'] = 'true'
+//   const np = process.execPath
+//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
+//   const options: cp.ExecFileSyncOptions = {
+//     env: process.env
+//   }
+//   console.log(cp.execFileSync(np, [ip], options).toString())
+// })
---
+++ src/report.ts
@@ -0,0 +1,131 @@
+import * as service from './service';
+import q from 'q';
+import FormData from 'form-data';
+import fs from 'fs';
+import url from 'url';
+import * as core from '@actions/core';
+
+export interface ReportMetaData {
+    dtpBuildId: string,
+    dtpSessionTag: string,
+    dtpProject: string,
+    appendEnvironmentSet : boolean
+}
+
+export class ReportPublisher extends service.WebService {
+    private ctpService: service.WebService;
+    private metaData: ReportMetaData;
+
+    constructor(
+        endpoint: string, 
+        context: string, 
+        ctpService: service.WebService,
+        metaData : ReportMetaData,
+        authorization?: service.Authorization) 
+    {
+        super(endpoint, context, authorization)
+        this.ctpService = ctpService;
+    }
+    
+
+    uploadFile<T>(reportId: number): q.Promise<T>{
+        let def = q.defer<T>();
+        this.performGET('/api/v1.6/services').then((response: any) => {
+            let dataCollectorURL = url.parse(response.services.dataCollectorV2);
+            let form = new FormData();
+            let protocol: 'https:' | 'http:' = dataCollectorURL.protocol === 'https:' ? 'https:' : 'http:';
+            form.append('file', fs.createReadStream(`${reportId}/report.xml`));
+            let options: FormData.SubmitOptions = {
+                host: dataCollectorURL.hostname,
+                port: parseInt(dataCollectorURL.port),
+                path: dataCollectorURL.path,
+                method: 'POST',
+                protocol: protocol,
+                headers: form.getHeaders()
+            };
+            if (protocol === 'https:') {
+                options['rejectUnauthorized'] = false;
+                options['agent'] = false;
+                if (this.authorization && this.authorization['username']) {
+                    options.auth = this.authorization['username'] + ':' + this.authorization['password'];
+                }
+            }
+            core.debug(`POST ${options.protocol}//${options.host}${options.port ? `:${options.port}` : ""}${options.path}`);
+            form.submit(options, (err, res) => {
+                if (err) {
+                    return def.reject(new Error(err.message));
+                }
+                if (res.statusCode < 200 || res.statusCode > 299) {
+                    return def.reject(new Error(`HTTP status code ${res.statusCode}`));
+                }
+                let body = [];
+                res.on('data', (chunk) => body.push(chunk));
+                res.on('end', () => {
+                    let resString: any = Buffer.concat(body).toString();
+                    def.resolve(resString);
+                });
+            });
+        }).catch((error) => {
+            def.reject(error);
+        });
+        return def.promise;
+    }
+
+   public publishReport(reportId: number, index: number, environmentName?: string): q.Promise<void> {
+        let def = q.defer<void>(),
+            firstCallback = true;
+        this.ctpService.performGET(`/testreport/${reportId}/report.xml`, (res, def, responseStr) => {
+            def.resolve(responseStr);
+        },
+            (response) => {
+                let fileData = response;
+                if (firstCallback) {
+                    fileData = this.injectMetaData(fileData, index, this.metaData.appendEnvironmentSet ? environmentName : null);
+                    firstCallback = false;
+                }
+                if (!fs.existsSync(`${reportId}`)){
+                    fs.mkdirSync(`${reportId}`);
+                }
+                fs.appendFile(`${reportId}/report.xml`, fileData, (error) => {
+                    if (error) {
+                        core.error(`Error writing report.xml: ${error.message}`);
+                        throw error;
+                    }
+                });
+                return '';
+            }).then(() => {
+                core.debug(`    View Report:  ${this.ctpService.getBaseURL()}/testreport/${reportId}/report.html`);
+                this.uploadFile(reportId).then(response => {
+                    core.debug(`   report.xml file upload successful: ${response}`);
+                    core.debug(`   View Result in DTP: ${this.getBaseURL()}/dtp/explorers/test?buildId=${this.metaData.dtpBuildId}`);
+                }).catch((error) => {
+                    core.error(`Error while uploading report.xml file: ${error}`);
+                });
+            });
+        return def.promise;
+    }
+
+    injectMetaData(source: string, index: number, environmentName?: string): string {
+        let dtpSessionTag = this.metaData.dtpSessionTag;
+        if (environmentName) {
+            if (dtpSessionTag == null) {
+                dtpSessionTag = "";
+            }
+            if (dtpSessionTag.length != 0) {
+                dtpSessionTag += '-';
+            }
+            dtpSessionTag += environmentName;
+        } else if ((dtpSessionTag != null) && dtpSessionTag.length !== 0 && (index > 0)) {
+            dtpSessionTag += `-${index + 1}`; // unique session tag in DTP for each report
+        }
+        source = this.replaceAttributeValue(source, 'project', this.metaData.dtpProject);
+        source = this.replaceAttributeValue(source, 'buildId', this.metaData.dtpBuildId);
+        source = this.replaceAttributeValue(source, 'tag', dtpSessionTag);
+        return this.replaceAttributeValue(source, 'execEnv', environmentName);
+    }
+
+    replaceAttributeValue(source: string, attribute: string, newValue: string): string {
+        let regEx = new RegExp(attribute + '\=\"[^"]*\"');
+        return source.replace(regEx, attribute + '="' + newValue + '"');
+    }
+}
